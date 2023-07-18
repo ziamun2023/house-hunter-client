b4ef78d927b31722d76d8272be778131dd941be7
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-07-19 02:20:03 +0600

    section of allapartment has been done

--- src/componenet/AllApartment/AllApartment.jsx
+++ src/componenet/AllApartment/AllApartment.jsx
@@ -3,6 +3,14 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../../AuthProvider';
 import ResuableTitle from '../ReusableTitle/ResuableTitle';
 
+import { GrLocation } from 'react-icons/Gr';
+import { LuBedDouble } from 'react-icons/Lu';
+import { FaShower } from 'react-icons/Fa';
+import { FaRegBuilding } from 'react-icons/Fa';
+import { BsSuitHeart } from 'react-icons/Bs';
+// import { BiHeart } from 'react-icons/Bi';
+import { BsCashCoin } from 'react-icons/Bs';
+
 const AllApartment = () => {
     
     const {user}=useContext(AuthContext)
@@ -13,14 +21,60 @@ const AllApartment = () => {
       
 
     return (
-        <div className='mt-20'>
+        <div className='mt-20 lg:mx-20'>
           <ResuableTitle title={'All popular Apartments'} subtitle={"Find Your Best choice"}></ResuableTitle>
 
-          <div className='grid lg:grid-cols-3'>
+          <div className='grid gap-10 lg:grid-cols-3'>
             {
+products.slice(0,6).map(c=><div className='rounded-xl relative bg-white shadow-lg' key={c._id}>
+    <div>
+        <img className='w-[500px] h-[270px] object-cover ' src={c.picture} alt="" />
+    </div>
+    <div className='ms-5'>
+        <p className='text-[25px] text-gray-800 font-bold tracking-wider '>{c.name}</p>
+    <div className='flex'>
+    <GrLocation size={20} className='text-indigo-600 '/>   <p className='text-[13px] text-gray-400 font-medium tracking-wider '>{c.address}</p>
+    </div>
+    </div>
+    <div className='mx-12 grid grid-cols-3 py-4 px-6 my-3 bg-[rgb(247,246,255)] '>
+        <p className='flex'>
+    <div>
+    <LuBedDouble size={20} className='text-indigo-600 me-2'/> </div> <div className='text-gray-900'>      {c.bedrooms}</div>
+        </p>
+        <p  className='flex'>
+       <div>   <FaShower size={20} className='text-indigo-600 me-2'/> </div><div className='text-gray-900' > {c.Bathroom}
+     
+       </div>
+        </p>
+        <p  className='flex'>
+        <div>
+        <FaRegBuilding size={20} className='text-indigo-600 me-2'/></div>   <div className='text-gray-900'>
+        {c.roomsize}
+       </div>
+        </p>
 
+     
+    </div>
+    <div className='mx-2 flex justify-around my-5'>
+        <p className='text-gray-600'><b>Listed on:</b>{c?.date}</p>
+        <p className='text-gray-600'><b>Owner :</b>{c?.ownername}</p>
+        <p className='text-gray-600'><b>View details </b></p>
+    </div>
+    <div className='bg-indigo-400 w-20 h-13 flex text-gray-800 font-semibold top-2 left-2 rounded-xl  absolute'>
+<div>     <BsCashCoin className='pt-2' size={24}/>  </div> <div className=''> 
+{c.rent}
+</div>
+         
+    </div>
+    <div className='hover:bg-indigo-400 w-10 h-10 flex text-gray-800 font-semibold top-2 right-2 rounded-full  absolute'>
+    <BsSuitHeart className='text-gray-800 p-1' size={50}/>  
+         
+    </div>
+
+</div>)
             }
           </div>
+         
         </div>
     );
 };
--- src/componenet/section 1/Section2.jsx
+++ src/componenet/section 1/Section2.jsx
@@ -1,9 +1,94 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { ImOffice } from 'react-icons/Im';
+import { ImHome } from 'react-icons/Im';
+import { GiPostOffice } from 'react-icons/Gi';
+import { GiVillage } from 'react-icons/Gi';
+
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+import ResuableTitle from '../ReusableTitle/ResuableTitle';
 const Section2 = () => {
     return (
-        <div className='h-[500px] bg-indigo-600'>
-            
+        <div className='h-[400px] px-10 mt-20 grid grid-cols-1 lg:grid-cols-4 bg-indigo-800'>
+            <div className='lg:col-span-2 mt-6'>
+              <p className='text-white text-[40px] font-semibold'>Explore By property</p>
+             
+           <div className='flex gap-10 mt-6'>
+           <p className='bg-red-600 h-2 w-10 rounded-lg font-semibold'></p>
+           <p className='bg-red-600 h-2 w-14 rounded-lg font-semibold'></p>
+           </div>
+           <p className='text-gray-200 text-[20px] mt-5'>Experience Hassle-Free Renting: Your Gateway to Quality Homes</p>
+
+            </div>
+            <div className=' lg:col-span-2 my-auto'>
+            <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide >
+            <div className='bg-white  justify-items-center  h-[140px] lg:h-[300px] grid rounded-lg' >
+           
+               <div> 
+               <ImOffice className='lg:mt-20 mt-10  text-black' size={70}/>
+               </div>
+              <div>
+              <p className='text-black font-semibold text-center'>Appartment</p>
+                <p className='text-gray-400'>25 properties</p>
+              </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className='bg-white  justify-items-center  h-[140px] lg:h-[300px] grid rounded-lg'>
+          
+
+          <div>
+          <ImHome className='lg:mt-20 mt-10  text-black' size={70}/>
+          </div>
+              <div>
+              <p className='text-black font-semibold text-center'>House</p>
+                <p className='text-gray-400'>40 properties</p>
+              </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className='bg-white  justify-items-center  h-[140px] lg:h-[300px] grid rounded-lg'>
+          
+             <div>
+             <GiPostOffice className='lg:mt-20 mt-10  text-black' size={70}/>
+             </div>
+             <div>
+             <p className='text-black font-semibold text-center'>Office</p>
+                <p className='text-gray-400'>40 properties</p>
+             </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className='bg-white  justify-items-center  h-[140px] lg:h-[300px] grid rounded-lg'>
+           
+            <div>
+            <GiVillage className='lg:mt-20 mt-10  text-black' size={70}/>
+            </div>
+              <div>
+              <p className='text-black font-semibold text-center'>Villa</p>
+                <p className='text-gray-400'>40 properties</p>
+              </div>
+            </div>
+        </SwiperSlide>
+       
+      </Swiper>
+            </div>
         </div>
     );
 };
